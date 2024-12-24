2963d39c5a16a14ac8142eed80a185eb1586693f
block2k <duclong2kzz@gmail.com>
2024-12-24 20:32:43 +0700

    Add authentication layout and pages, update dependencies, and remove unused files

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,8 @@
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <main>
+      SIDEBAR
+      {children}
+    </main>
   );
 }
